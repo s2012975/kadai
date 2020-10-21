139c064ece81ac1433a48994218e1d15d289fc11
s2012975 <bdips2012@gmail.com>
2020-10-21 17:35:31 +0900

    [Finish] 2-7

--- rails_chapter7/sample_app/config/initializers/application_controller_renderer.rb
+++ rails_chapter7/sample_app/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+
+Refile.secret_key = 'a7cc270126129ec359d9d4cc802cbaf59ed55f91b02107dcb16ac55c086a277446a19af994a2e8373fb81e572e5b3502a8c4caae00950d946a5909164f5e1a82'
