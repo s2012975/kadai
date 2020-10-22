70ea63884c9c181a849d5e6beeba02b2b0630963
s2012975 <bdips2012@gmail.com>
2020-10-22 20:19:16 +0900

    [Start] 2-10

---
+++ rails_chapter10/sample_app/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,11 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+
+Refile.secret_key = 'a7cc270126129ec359d9d4cc802cbaf59ed55f91b02107dcb16ac55c086a277446a19af994a2e8373fb81e572e5b3502a8c4caae00950d946a5909164f5e1a82'
