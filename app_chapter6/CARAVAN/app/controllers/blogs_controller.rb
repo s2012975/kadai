919a3ce4d895c95c7163dc1e924e741ac5ae6950
s2012975 <bdips2012@gmail.com>
2020-10-25 13:21:13 +0900

    [Finish] 3-6

--- app_chapter6/CARAVAN/app/controllers/blogs_controller.rb
+++ app_chapter6/CARAVAN/app/controllers/blogs_controller.rb
@@ -17,7 +17,14 @@ class BlogsController < ApplicationController
     redirect_to blog_path(blog.id)
   end
 
+  def update
+    blog = Blog.find(params[:id])
+    blog.update(blog_params)
+    redirect_to blog_path(blog)
+  end
+
   def edit
+    @blog = Blog.find(params[:id])
   end
 
   private
