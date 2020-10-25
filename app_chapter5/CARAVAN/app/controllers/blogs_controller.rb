eef31455b0bd709fc06e42382a214b4bda14e645
s2012975 <bdips2012@gmail.com>
2020-10-25 11:51:31 +0900

    [Finish] 3-5

--- app_chapter5/CARAVAN/app/controllers/blogs_controller.rb
+++ app_chapter5/CARAVAN/app/controllers/blogs_controller.rb
@@ -4,16 +4,17 @@ class BlogsController < ApplicationController
   end
 
   def show
+    @blog = Blog.find(params[:id])
   end
 
   def new
     @blog = Blog.new
   end
 
-  def crete
+  def create
     blog = Blog.new(blog_params)
     blog.save
-    redirect_to blogs_path
+    redirect_to blog_path(blog.id)
   end
 
   def edit
