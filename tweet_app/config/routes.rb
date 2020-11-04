74dbf0f08302a213327887b7bff03e827bcf0b1c
s2012975 <bdips2012@gmail.com>
2020-11-04 23:25:11 +0900

    [Finish] rails-5

--- tweet_app/config/routes.rb
+++ tweet_app/config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get "login" => "users#login_form"
+  post "login" => "users#login"
+  
+  post "logout" => "users#logout"
+  
+  post "users/:id/update" => "users#update"
+  get "users/:id/edit" => "users#edit"
+  post "users/create" => "users#create"
+  get "signup" => "users#new"
+  get "users/index" => "users#index"
+  get "users/:id" => "users#show"
+
   get "posts/index" => "posts#index"
   get "posts/new" => "posts#new"
   get "posts/:id" => "posts#show"
--- tweet_app/db/schema.rb
+++ tweet_app/db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170331045923) do
+ActiveRecord::Schema.define(version: 20201103032707) do
 
   create_table "posts", force: :cascade do |t|
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "image_name"
+    t.string   "password"
   end
 
 end
