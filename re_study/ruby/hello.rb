292131c5f96b1a45bd94488cb76d77851d5d76bb
s2012975 <bdips2012@gmail.com>
2020-11-14 17:15:43 +0900

    [Finish] ruby

--- re_study/ruby/hello.rb
+++ re_study/ruby/hello.rb
@@ -1,3 +1,21 @@
-puts "何か打ち込んで"
-input_key = gets.chomp
-puts "打ち込まれたのは#{input_key}"
+def fizz_buzz(number)
+  puts "結果は…"
+  if number % 15 == 0
+    puts "FizzBuzz"
+  elsif number % 5 == 0
+    puts "Buzz"
+  elsif number % 3 == 0
+    puts "Fizz"
+  else
+    number = number.to_s
+    puts number
+  end
+end
+
+n = gets.to_i
+
+fizz_buzz(n)
+
+for i in 1..100 do
+  fizz_buzz(i)
+end
